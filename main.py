937fe1adba5e88f28fd9765427380bd17b6c8764
ashesh808 <37954173+ashesh808@users.noreply.github.com>
2025-09-27 20:48:35 -0500

    feat: upgraded z8ter to 0.2.4 and cleaned up the templates

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-from z8ter.core import Z8ter
-from z8ter.config import build_config
+from z8ter.builders.app_builder import AppBuilder
 
-config = build_config(".env")
-
-
-def create_app() -> Z8ter:
-    app = Z8ter(
-        debug=False,
-        mode=config.get('Z8_MODE', default="dev")
-        )
-    return app
-
+app_builder = AppBuilder()
+app_builder.use_config(".env")
+app_builder.use_templating()
+app_builder.use_vite()
+app_builder.use_errors()
 
 if __name__ == "__main__":
-    app = create_app()
+    app = app_builder.build(debug=True)
