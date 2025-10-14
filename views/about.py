60e7516b3e5a87836a1a4369a316220eeb83dbdc
Ashesh Nepal <37954173+ashesh808@users.noreply.github.com>
2025-10-14 18:28:41 -0500

    fix: removed legacy views

--- views/about.py
+++
@@ -1,8 +0,0 @@
-from z8ter.page import Page
-from z8ter.requests import Request
-from z8ter.responses import Response
-
-
-class About(Page):
-    async def get(self, request: Request) -> Response:
-        return self.render(request, "about.jinja", {})
--- views/index.py
+++
@@ -1,9 +0,0 @@
-from z8ter.page import Page
-from z8ter.requests import Request
-from z8ter.responses import Response
-
-
-class Index(Page):
-    async def get(self, request: Request) -> Response:
-        data = {"title": "Welcome to Z8ter!"}
-        return self.render(request, "index.jinja", data)
